e8ac0955448f0be0e0fa9323d5535b5f155f9126
huongtravo0910 <huongtravo0910@gmail.com>
2020-10-03 15:49:50 +0700

    Updates

---
+++ precache-manifest.18c7a50c66b06a2414c0fdfa8f216bf6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d94bb898fa5af7e5801df96e3815694",
+    "url": "/huongtravo0910/react-netflix-clone.git/index.html"
+  },
+  {
+    "revision": "9527e673c1300db56a58",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/css/main.85c44fdf.chunk.css"
+  },
+  {
+    "revision": "85dfc5d3bd4045ae80e0",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/js/2.e2fc4c1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/js/2.e2fc4c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9527e673c1300db56a58",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/js/main.6e2f7ea3.chunk.js"
+  },
+  {
+    "revision": "bd26a98b0c97cb769c3e",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/js/runtime-main.18e3c833.js"
+  },
+  {
+    "revision": "9dfd33c96d854adf571b4f4c56af6c8c",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/media/Oceans-8-movie-wallpaper.9dfd33c9.jpg"
+  },
+  {
+    "revision": "9cf134bf0ea8b16aa7d270076c107caa",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/media/logo-remove.9cf134bf.png"
+  },
+  {
+    "revision": "e87a548f648a6c28f2c5086198a3bed0",
+    "url": "/huongtravo0910/react-netflix-clone.git/static/media/oceans-8.e87a548f.jpg"
+  }
+]);
